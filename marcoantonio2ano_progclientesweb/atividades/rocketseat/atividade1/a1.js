49792aa6c5e4c45cc0040c9ab2207ddd04c44071
marcoarnetto <57268587+marcoarnetto@users.noreply.github.com>
2020-12-13 20:04:09 -0300

    updates 13/12

---
+++ marcoantonio2ano_progclientesweb/atividades/rocketseat/atividade1/a1.js
@@ -0,0 +1,9 @@
+var e = {  
+    0: "Rua dos pinheiros",  
+    1: 1293, 
+    2: "Centro",  
+    3: "São Paulo",  
+    4: "SP"
+}; 
+   document.getElementById("retorno").innerHTML = "O usuário mora em "+e[3]+ 
+   " / " +e[4]+ ", no bairro "+e[2]+", na rua "+e[0]+" comnº "+e[1]+".";
---
+++ marcoantonio2ano_progclientesweb/atividades/rocketseat/atividade2/a2.js
@@ -0,0 +1,17 @@
+ function chamaF(){
+var a = document.getElementById('e1').value; 
+var b = document.getElementById('e2').value; 
+
+a = parseInt(a);
+b = parseInt(b);
+
+var i;
+
+function pares (i, b){
+    for(i; i<=b; i++) {
+        if(i%2==0) {
+            console.log(i+"<br>");
+        }
+    }
+}
+}
