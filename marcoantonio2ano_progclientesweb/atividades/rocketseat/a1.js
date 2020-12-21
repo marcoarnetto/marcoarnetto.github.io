2e828d6411d94570b40648099fbadddb20254ff6
marcoarnetto <57268587+marcoarnetto@users.noreply.github.com>
2020-12-21 14:54:49 -0300

    .

--- marcoantonio2ano_progclientesweb/atividades/rocketseat/a1.js
+++ marcoantonio2ano_progclientesweb/atividades/rocketseat/a1.js
@@ -6,9 +6,12 @@
     2: "Centro",  
     3: "São Paulo",  
     4: "SP"
-}; 
-   var retorno=document.getElementById("retorno").innerHTML = "O usuário mora em "+e[3]+ 
-   " / " +e[4]+ ", no bairro "+e[2]+", na rua "+e[0]+" comnº "+e[1]+".";
-   var resultStr = document.createTextNode(retorno);
+}
+   retor = "O usuário mora em "+e[3]+" / " +e[4]+ ", no bairro "+e[2]+", na rua "+e[0]+" comnº "+e[1]+".";
+   var retorno=document.getElementById("retorno");
+   var r=retor;
+   var resultStr = document.createTextNode(r);
+   retorno.appendChild(resultStr)
+   retorno.innerHTML = "";
    retorno.appendChild(resultStr);
- }
+}
---
+++ marcoantonio2ano_progclientesweb/atividades/rocketseat/a3.js
@@ -0,0 +1,13 @@
+function chamaFuncao(){
+var a;
+var skills = ["Javascript", "ReactJS", "React Native"]/
+
+function temHabilidade(skills){
+    a=skills.indexOf("Javascript");
+}
+if (a=>0){
+    document.write("true");
+} else {
+    document.write("false");
+}
+}
